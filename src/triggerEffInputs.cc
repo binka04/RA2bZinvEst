fa1731b58572a82e07d8bd51745b9853f34e4304
awhitbeck <whitbeck.andrew@gmail.com>
2016-11-21 15:34:21 -0600

    commenting out plotting stuff which seemed to intermittenly cause jobs on condor to crash

--- src/triggerEffInputs.cc
+++ src/triggerEffInputs.cc
@@ -20,62 +20,63 @@
 
 using namespace std;
 
-/////////////////////////////////////////////////////////////////
-//  - - - - - - - - Triggers for V10 ntuples - - - - - - - - - //
-// 0 HLT_CaloJet500_NoJetID_v2				       //
-// 1 HLT_DoubleEle8_CaloIdM_TrackIdM_Mass8_PFHT300_v4	       //
-// 2 HLT_DoubleMu8_Mass8_PFHT300_v4			       //
-// 3 HLT_Ele105_CaloIdVT_GsfTrkIdT_v3			       //
-// 4 HLT_Ele115_CaloIdVT_GsfTrkIdT_v2			       //
-// 5 HLT_Ele15_IsoVVVL_PFHT350_PFMET50_v2		       //
-// 6 HLT_Ele15_IsoVVVL_PFHT350_v2			       //
-// 7 HLT_Ele15_IsoVVVL_PFHT400_v			       //
-// 8 HLT_Ele15_IsoVVVL_PFHT600_v3			       //
-// 9 HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v3	       //
-// 10 HLT_Ele25_eta2p1_WPTight_Gsf_v 			       //
-// 11 HLT_Ele27_WPTight_Gsf_v1				       //
-// 12 HLT_Ele27_eta2p1_WPLoose_Gsf_v 			       //
-// 13 HLT_Ele45_WPLoose_Gsf_v1				       //
-// 14 HLT_Ele50_IsoVVVL_PFHT400_v			       //
-// 15 HLT_IsoMu16_eta2p1_MET30_v1			       //
-// 16 HLT_IsoMu22_eta2p1_v				       //
-// 17 HLT_IsoMu22_v2					       //
-// 18 HLT_IsoMu24_v 					       //
-// 19 HLT_IsoTkMu22_v2					       //
-// 20 HLT_Mu15_IsoVVVL_PFHT350_PFMET50_v2		       //
-// 21 HLT_Mu15_IsoVVVL_PFHT350_v2			       //
-// 22 HLT_Mu15_IsoVVVL_PFHT400_v			       //
-// 23 HLT_Mu15_IsoVVVL_PFHT600_v3			       //
-// 24 HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v2		       //
-// 25 HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v2		       //
-// 26 HLT_Mu45_eta2p1_v2				       //
-// 27 HLT_Mu50_IsoVVVL_PFHT400_v			       //
-// 28 HLT_Mu50_v2					       //
-// 29 HLT_PFHT200_v2					       //
-// 30 HLT_PFHT250_v2					       //
-// 31 HLT_PFHT300_PFMET100_v1				       //
-// 32 HLT_PFHT300_PFMET110_v1				       //
-// 33 HLT_PFHT300_v2					       //
-// 34 HLT_PFHT350_v3					       //
-// 35 HLT_PFHT400_v2					       //
-// 36 HLT_PFHT475_v2					       //
-// 37 HLT_PFHT600_v3					       //
-// 38 HLT_PFHT650_v3					       //
-// 39 HLT_PFHT800_v2					       //
-// 40 HLT_PFHT900_v1					       //
-// 41 HLT_PFMET100_PFMHT100_IDTight_v2			       //
-// 42 HLT_PFMET110_PFMHT110_IDTight_v2			       //
-// 43 HLT_PFMET120_PFMHT120_IDTight_v2			       //
-// 44 HLT_PFMET90_PFMHT90_IDTight_v2			       //
-// 45 HLT_PFMETNoMu100_PFMHTNoMu100_IDTight_v1		       //
-// 46 HLT_PFMETNoMu110_PFMHTNoMu110_IDTight_v1		       //
-// 47 HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v1		       //
-// 48 HLT_PFMETNoMu90_PFMHTNoMu90_IDTight_v1		       //
-// 49 HLT_Photon165_HE10_v3				       //
-// 50 HLT_Photon175_v3					       //
-// 51 HLT_Photon90_CaloIdL_PFHT500_v3			       //
-// 52 HLT_TkMu50_v					       //
-/////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//  - - - -  - - - - Prduction V11 Trigger Indices - - - - - - - - - -  //
+// 0 HLT_CaloJet500_NoJetID_v5                                          //
+// 1 HLT_DoubleEle8_CaloIdM_TrackIdM_Mass8_PFHT300_v10                  //
+// 2 HLT_DoubleMu8_Mass8_PFHT300_v9                                     //
+// 3 HLT_Ele105_CaloIdVT_GsfTrkIdT_v8                                   //
+// 4 HLT_Ele115_CaloIdVT_GsfTrkIdT_v7                                   //
+// 5 HLT_Ele15_IsoVVVL_PFHT350_PFMET50_v                                //
+// 6 HLT_Ele15_IsoVVVL_PFHT350_v                                        //
+// 7 HLT_Ele15_IsoVVVL_PFHT400_v6                                       //
+// 8 HLT_Ele15_IsoVVVL_PFHT600_v9                                       //
+// 9 HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v9                       //
+// 10 HLT_Ele25_eta2p1_WPTight_Gsf_v                                    //
+// 11 HLT_Ele27_WPTight_Gsf_v7                                          //
+// 12 HLT_Ele27_eta2p1_WPLoose_Gsf_v                                    //
+// 13 HLT_Ele45_WPLoose_Gsf_v                                           //
+// 14 HLT_Ele50_IsoVVVL_PFHT400_v6                                      //
+// 15 HLT_IsoMu16_eta2p1_MET30_v4                                       //
+// 16 HLT_IsoMu22_eta2p1_v4                                             //
+// 17 HLT_IsoMu22_v5                                                    //
+// 18 HLT_IsoMu24_v                                                     //
+// 19 HLT_IsoTkMu22_v5                                                  //
+// 20 HLT_IsoTkMu24_v4                                                  //
+// 21 HLT_Mu15_IsoVVVL_PFHT350_PFMET50_v                                //
+// 22 HLT_Mu15_IsoVVVL_PFHT350_v                                        //
+// 23 HLT_Mu15_IsoVVVL_PFHT400_v5                                       //
+// 24 HLT_Mu15_IsoVVVL_PFHT600_v8                                       //
+// 25 HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v6                               //
+// 26 HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v5                             //
+// 27 HLT_Mu45_eta2p1_v5                                                //
+// 28 HLT_Mu50_IsoVVVL_PFHT400_v5                                       //
+// 29 HLT_Mu50_v5                                                       //
+// 30 HLT_PFHT200_v6                                                    //
+// 31 HLT_PFHT250_v6                                                    //
+// 32 HLT_PFHT300_PFMET100_v                                            //
+// 33 HLT_PFHT300_PFMET110_v6                                           //
+// 34 HLT_PFHT300_v7                                                    //
+// 35 HLT_PFHT350_v8                                                    //
+// 36 HLT_PFHT400_v7                                                    //
+// 37 HLT_PFHT475_v7                                                    //
+// 38 HLT_PFHT600_v8                                                    //
+// 39 HLT_PFHT650_v8                                                    //
+// 40 HLT_PFHT800_v                                                     //
+// 41 HLT_PFHT900_v6                                                    //
+// 42 HLT_PFMET100_PFMHT100_IDTight_v                                   //
+// 43 HLT_PFMET110_PFMHT110_IDTight_v7                                  //
+// 44 HLT_PFMET120_PFMHT120_IDTight_v7                                  //
+// 45 HLT_PFMET90_PFMHT90_IDTight_v                                     //
+// 46 HLT_PFMETNoMu100_PFMHTNoMu100_IDTight_v                           //
+// 47 HLT_PFMETNoMu110_PFMHTNoMu110_IDTight_v7                          //
+// 48 HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v7                          //
+// 49 HLT_PFMETNoMu90_PFMHTNoMu90_IDTight_v                             //
+// 50 HLT_Photon165_HE10_v8                                             //
+// 51 HLT_Photon175_v8                                                  //
+// 52 HLT_Photon90_CaloIdL_PFHT500_v                                    //
+// 53 HLT_TkMu50_v3                                                     //
+//////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char** argv){
 
@@ -85,8 +86,8 @@ int main(int argc, char** argv){
     cout << "Arg. 2 - last file index" << endl;
   }
 
-  gROOT->ProcessLine(".L ~/tdrstyle.C");
-  gROOT->ProcessLine("setTDRStyle()");
+  //gROOT->ProcessLine(".L ~/tdrstyle.C");
+  //gROOT->ProcessLine("setTDRStyle()");
 
   typedef plot<RA2bTree> plot;
   
@@ -167,23 +168,23 @@ int main(int argc, char** argv){
     int numEvents = ntuple->fChain->GetEntries();
     ntupleBranchStatus<RA2bTree>(ntuple);
     for( int iEvt = 0 ; iEvt < numEvents ; iEvt++ ){
-      ntuple->GetEntry(iEvt);
-      if( iEvt % 1000000 == 0 ) cout << sampleNames[iSample] << ": " << iEvt << "/" << numEvents << endl;
-
-      if( ntuple->TriggerPass->size() <= 50 ) continue;      
-      if( ntuple->TriggerPass->at(40) != 1 && ntuple->TriggerPass->at(39) != 1 && ntuple->TriggerPass->at(38) != 1 && ntuple->TriggerPass->at(37) != 1 && ntuple->TriggerPass->at(36) != 1 && ntuple->TriggerPass->at(35) != 1 && ntuple->TriggerPass->at(34) != 1 && ntuple->TriggerPass->at(33) != 1 ) continue;
-      if( ntuple->Photons->size() == 0 ) continue;
-      if( ntuple->Photons->at(0).Pt() < 100. ) continue;
-
-      for( int iPlot = 0 ; iPlot < plotsTarget.size() ; iPlot++ ){
-	plotsReference[iPlot].fillData(ntuple);
-	if( ntuple->TriggerPass->at(50) == 1 ){
-	  plotsTarget[iPlot].fillData(ntuple);
-	}
-      }
+        ntuple->GetEntry(iEvt);
+        if( iEvt % 1000000 == 0 ) cout << sampleNames[iSample] << ": " << iEvt << "/" << numEvents << endl;
+        
+        if( ntuple->TriggerPass->size() <= 51 ) continue;      
+        if( ntuple->TriggerPass->at(41) != 1 && ntuple->TriggerPass->at(40) != 1 && ntuple->TriggerPass->at(39) != 1 && ntuple->TriggerPass->at(38) != 1 && ntuple->TriggerPass->at(37) != 1 && ntuple->TriggerPass->at(36) != 1 && ntuple->TriggerPass->at(35) != 1 && ntuple->TriggerPass->at(34) != 1 ) continue;
+        if( ntuple->Photons->size() == 0 ) continue;
+        if( ntuple->Photons->at(0).Pt() < 100. ) continue;
+        
+        for( int iPlot = 0 ; iPlot < plotsTarget.size() ; iPlot++ ){
+            plotsReference[iPlot].fillData(ntuple);
+            if( ntuple->TriggerPass->at(51) == 1 ){
+                plotsTarget[iPlot].fillData(ntuple);
+            }
+        }
     }
   }
-
+  
   TFile* outputFile = new TFile("triggerEff_"+TString(argv[1])+"_"+TString(argv[2])+"_histo.root","RECREATE");
   //TFile* outputFile = new TFile("triggerEff_histo.root","RECREATE");
 
@@ -192,6 +193,7 @@ int main(int argc, char** argv){
   for( int iPlot = 0 ; iPlot < plotsTarget.size() ; iPlot++){
     plotsTarget[iPlot].Write();
     plotsReference[iPlot].Write();
+    /*
     TGraphAsymmErrors* ratio = new TGraphAsymmErrors(plotsTarget[iPlot].dataHist,plotsReference[iPlot].dataHist);
     ratio->SetMarkerStyle(8);
     ratio->GetXaxis()->SetTitle(xlabels[iPlot]);
@@ -207,6 +209,7 @@ int main(int argc, char** argv){
     can->GetFrame()->Draw();
     
     can->SaveAs("triggerEff_plots/"+TString(plotsTarget[iPlot].dataHist->GetName())+".png");
+    */
   }
   outputFile->Close();
 }
