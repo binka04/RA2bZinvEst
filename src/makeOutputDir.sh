5a3cd0c064a0af79baa4c713811edb1c5f9f3816
awhitbeck <whitbeck.andrew@gmail.com>
2018-06-26 12:29:06 -0500

    remove use of ~/

--- src/makeOutputDir.sh
+++ src/makeOutputDir.sh
@@ -2,18 +2,18 @@
 
 newDir=$1
 
-if [ -d ~/eos/RA2bZinvEst/$newDir ] 
+if [ -d /eos/uscms/store/user/$USER/RA2bZinvEst/$newDir ] 
     then
     echo "DIRECTORY ALREADY EXISTS -- I DON'T KNOW HOW TO HANDLE THIS...BYE"
     exit 1
 fi
 
-mkdir ~/eos/RA2bZinvEst/$newDir
-mkdir ~/eos/RA2bZinvEst/$newDir/plots
+mkdir /eos/uscms/store/user/$USER/RA2bZinvEst/$newDir
+mkdir /eos/uscms/store/user/$USER/RA2bZinvEst/$newDir/plots
 plotsDirList="Properties_plots RzGamma_DR0p05_plots RzGamma_plots fragmentationFits plotObs_baseline_plots plotPurityProperties_plots purityFits purityResults triggerEff_plots"
 for i in $plotsDirList 
 do 
-    mkdir ~/eos/RA2bZinvEst/$newDir/plots/$i
+    mkdir /eos/uscms/store/user/$USER/RA2bZinvEst/$newDir/plots/$i
 done
 
 
